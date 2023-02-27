48f15c87dbe53b0b102da2a864979841f1bcc1ca
Nikita <nikitosskk97@gmail.com>
2023-02-28 00:59:31 +0300

    added program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] mainArray = { "hello", "2", "world", ":-)" };
+int sizeMainArray = mainArray.Length;
+string[] resultArray = new string[sizeMainArray];
+int sizeResultArray = 0;
+
+for (int i = 0; i < sizeMainArray; i++)
+{
+    if (mainArray[i].Length <= 3)
+    {
+        resultArray[sizeResultArray] = mainArray[i];
+        sizeResultArray += 1;
+    }
+}
+
+Console.Write("[");
+Console.Write(string.Join(", ", resultArray, 0, sizeResultArray));
+Console.Write("]");
